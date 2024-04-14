fc2caa791f4dad463ca7c18ab809b213987e45b8
andreabellome <andrea.bellome@gmail.com>
2024-04-14 16:08:08 +0200

    upd and new results

--- test_v2.py
+++ test_v2.py
@@ -2,19 +2,12 @@
 # ==============================================================================
 import numpy as np
 import pandas as pd
-from astral.sun import sun
-from astral import LocationInfo
-from skforecast.datasets import fetch_dataset
 
 # Plots
 # ==============================================================================
 import matplotlib.pyplot as plt
-from statsmodels.graphics.tsaplots import plot_acf
-from statsmodels.graphics.tsaplots import plot_pacf
-import plotly.graph_objects as go
 import plotly.io as pio
 import plotly.offline as poff
-import plotly.offline as pyo
 
 pio.renderers.default = 'notebook' 
 pio.templates.default = "seaborn"
@@ -23,14 +16,7 @@ plt.style.use('seaborn-v0_8-darkgrid')
 
 # Modelling and Forecasting
 # ==============================================================================
-from lightgbm import LGBMRegressor
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.feature_selection import RFECV
-from skforecast.ForecasterBaseline import ForecasterEquivalentDate
-from skforecast.ForecasterAutoreg import ForecasterAutoreg
-from skforecast.ForecasterAutoregDirect import ForecasterAutoregDirect
-from skforecast.model_selection import bayesian_search_forecaster
-from skforecast.model_selection import backtesting_forecaster
+from statsmodels.tsa.statespace.sarimax import SARIMAX
 import shap
 shap.initjs()
 
@@ -67,133 +53,35 @@ if data.isnull().any(axis=1).mean() > 0.0:
 # ==============================================================================
 data = data.loc['2006-01-01': '2019-12-31'].copy()
 start_train = '2006-01-01'
-end_train = '2017-12-31'
-start_validation = '2018-01-01'
-end_validation = '2018-12-31'
+end_train = '2018-12-31'
 start_test = '2019-01-01'
-data_train = data.loc[start_train:end_train, :].copy()
-data_val   = data.loc[start_validation:end_validation, :].copy()
-data_test  = data.loc[start_test:, :].copy()
+end_test = '2019-12-31'
 
-print(f"Train dates      : {data_train.index.min()} --- {data_train.index.max()}  (n={len(data_train)})")
-print(f"Validation dates : {data_val.index.min()} --- {data_val.index.max()}  (n={len(data_val)})")
-print(f"Test dates       : {data_test.index.min()} --- {data_test.index.max()}  (n={len(data_test)})")
+train = data.loc[:end_train, :].copy()
+test = data.loc[start_test:, :].copy()
 
-# Set the forecast horizon and the lags
-# ==============================================================================
-steps = 1
-lags = 1850 
+sarima_model = SARIMAX(train, order=(1, 1, 1), seasonal_order=(2, 1, 1, 365))
+sarima_model_fit = sarima_model.fit(disp=0)
+print(sarima_model_fit.summary())
 
-# Create forecaster
-# ==============================================================================
-forecaster = ForecasterAutoreg(
-                 regressor = LGBMRegressor(random_state=15926, verbose=-1),
-                 lags      = lags
-             )
+sarima_predictions = sarima_model_fit.predict(start=start_test, end=end_test)
 
-# Train forecaster
-# ==============================================================================
-forecaster.fit(y=data.loc[:end_validation, 'Load'])
-forecaster
+# Figure
+fig, ax = plt.subplots(figsize=(16,9), facecolor='w')
 
-# Backtesting
-# ==============================================================================
-metric, predictions = backtesting_forecaster(
-                          forecaster         = forecaster,
-                          y                  = data['Load'],
-                          steps              = steps,
-                          metric             = 'mean_absolute_error',
-                          initial_train_size = len(data.loc[:end_validation]),
-                          refit              = False,
-                          n_jobs             = 'auto',
-                          verbose            = True,
-                          show_progress      = True
-                      )
+ax.plot(test, label='Testing Set')
+ax.plot(sarima_predictions, label='Forecast')
 
-# Backtesting error
-# ==============================================================================
-print(f'Backtest error (MAE): {metric}')
+# Labels
+ax.set_title("Test vs Predictions", fontsize=15, pad=10)
+ax.set_ylabel("Number of orders", fontsize=12)
+ax.set_xlabel("Date", fontsize=12)
 
-# Plot predictions vs real value
-# ======================================================================================
-fig = go.Figure()
-trace1 = go.Scatter(x=data_test.index, y=data_test['Load'], name="to predict", mode="lines")
-trace2 = go.Scatter(x=predictions.index, y=predictions['pred'], name="prediction", mode="lines")
-fig.add_trace(trace1)
-fig.add_trace(trace2)
-fig.update_layout(
-    title="Real value vs predicted in test data (Wh)",
-    xaxis_title="Date time",
-    yaxis_title="Load",
-    width=800,
-    height=400,
-    margin=dict(l=20, r=20, t=35, b=20),
-    legend=dict(
-        orientation="h",
-        yanchor="top",
-        y=1.1,
-        xanchor="left",
-        x=0.001
-    )
-)
+# Legend & Grid
+ax.grid(linestyle=":", color='grey')
+ax.legend()
 
-# Hyperparameters search
-# ==============================================================================
-forecaster = ForecasterAutoreg(
-                 regressor = LGBMRegressor(random_state=15926, verbose=-1),
-                 lags      = lags, # This value will be replaced in the search
-             )
+plt.show()
 
-# Lags used as predictors
-lags_grid = [lags, [1000, 1500, 1850, 1900, 2000, 2500, 2800, 2900, 3000]]
-
-
-# Regressor hyperparameters search space
-def search_space(trial):
-    search_space  = {
-        'n_estimators'  : trial.suggest_int('n_estimators', 800, 1500, step=100),
-        'max_depth'     : trial.suggest_int('max_depth', 3, 10, step=1),
-        'learning_rate' : trial.suggest_float('learning_rate', 0.01, 0.5),
-        'reg_alpha'     : trial.suggest_float('reg_alpha', 0, 1, step=0.1),
-        'reg_lambda'    : trial.suggest_float('reg_lambda', 0, 1, step=0.1),
-    } 
-    return search_space
-
-# Perform the search
-results_search, frozen_trial = bayesian_search_forecaster(
-                                   forecaster         = forecaster,
-                                   y                  = data.loc[:end_validation, 'Load'],
-                                   steps              = steps,
-                                   metric             = 'mean_absolute_error',
-                                   search_space       = search_space,
-                                   lags_grid          = lags_grid,
-                                   initial_train_size = len(data[:end_train]),
-                                   refit              = False,
-                                   n_trials           = 100, # Increase for more exhaustive search
-                                   random_state       = 123,
-                                   return_best        = True,
-                                   n_jobs             = 'auto',
-                                   verbose            = False,
-                                   show_progress      = True
-                               )
-
-# Search results
-# ==============================================================================
-results_search.head(10)
-
-# Backtest final model on test data
-# ==============================================================================
-metric, predictions = backtesting_forecaster(
-                          forecaster         = forecaster,
-                          y                  = data['Load'],
-                          steps              = steps,
-                          metric             = 'mean_absolute_error',
-                          initial_train_size = len(data[:end_validation]),
-                          refit              = False,
-                          n_jobs             = 'auto',
-                          verbose            = False, # Change to True to see detailed information
-                          show_progress      = True
-                      )
-
-print(f"Backtest error (MAE): {metric:.2f}")
+st = 1
 
